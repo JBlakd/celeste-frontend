e9b2dfb7d0660e564ac44ef79110237117b0a476
Ivan Hu <ivanhu2246@gmail.com>
2025-05-09 13:26:58 +0000

    example-marble background

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer';
+import classes from './RootLayout.module.css';
 
 export default function RootLayout() {
   return (
-    <>
+    <div className={classes.backgroundWrap}>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
