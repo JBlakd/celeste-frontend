9158f014070236ec0a6408b67bfcf6963b01d1a4
Ivan Hu <ivanhu2246@gmail.com>
2025-06-06 12:24:18 +0000

    Add theme colour

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider } from '@mantine/core';
 import { BrowserRouter, useRoutes } from 'react-router-dom';
 import { routes } from './routes';
 
@@ -6,9 +6,28 @@ function AppRoutes() {
   return useRoutes(routes);
 }
 
+const theme = createTheme({
+  primaryColor: 'celesteGold',
+  colors: {
+    celesteGold: [
+      '#F4EDE6',
+      '#EADCCC',
+      '#DFCAB3',
+      '#D5B999',
+      '#CBA880',
+      '#BC9C79',
+      '#A68365',
+      '#8F6A50',
+      '#79543D',
+      '#63402D',
+    ],
+  },
+  primaryShade: { light: 5, dark: 7 },
+});
+
 export default function App() {
   return (
-    <MantineProvider defaultColorScheme="light">
+    <MantineProvider defaultColorScheme="light" theme={theme}>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
