e4c40ae8e86e7bf297efff5700e48d89aeafa3f0
Ivan Hu <ivanhu2246@gmail.com>
2025-06-12 13:23:46 +0000

    Basic carousel product gallery

--- src/components/Product/ProductImages.tsx
+++ src/components/Product/ProductImages.tsx
@@ -1,4 +1,5 @@
 import { Container, Image, Paper } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
 import type { Product } from '@typedefs/sanity';
 
 export default function ProductImages({ product }: { product: Product | null }) {
@@ -6,11 +7,32 @@ export default function ProductImages({ product }: { product: Product | null }) 
     return null;
   }
 
+  const mainImageUrl = product.image?.asset?.url;
+  const galleryImages = product.gallery?.filter((img) => img.asset?.url);
+
   return (
     <Container size="lg">
+      {/* Main image */}
       <Paper radius="sm" shadow="sm">
-        <Image src={product.image?.asset?.url} alt={product.title} mt="2rem" />
+        {mainImageUrl && <Image src={mainImageUrl} alt={product.title} mt="2rem" />}
       </Paper>
+
+      {/* Gallery carousel */}
+      {galleryImages && galleryImages.length > 0 && (
+        <Carousel
+          mt="xl"
+          slideSize="33.333333%"
+          slideGap="md"
+          emblaOptions={{ align: 'start', loop: true, dragFree: true }}
+          withIndicators
+        >
+          {galleryImages.map((img, index) => (
+            <Carousel.Slide key={index}>
+              <Image src={img.asset?.url} alt={`${product.title} image ${index + 1}`} radius="sm" />
+            </Carousel.Slide>
+          ))}
+        </Carousel>
+      )}
     </Container>
   );
 }
