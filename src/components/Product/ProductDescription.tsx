cda0d3f3611454f074a1333e1087555d51ad00e4
Ivan Hu <ivanhu2246@gmail.com>
2025-06-19 04:37:08 +0000

    ProductDescription isMobile adjustments

--- src/components/Product/ProductDescription.tsx
+++ src/components/Product/ProductDescription.tsx
@@ -1,29 +1,30 @@
 import { Box, Container, Flex, Text, Title, useMantineTheme } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import type { Product } from '@typedefs/sanity';
 
 export default function ProductDescription({ product }: { product: Product }) {
   const theme = useMantineTheme();
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
   return (
     <Box
       style={{
         width: '100%',
       }}
-      px="2rem"
     >
       <Container size="lg" py="xl">
         <Flex gap="xs">
-          <Title order={2} c={theme.colors.celesteGold[5]}>
+          <Title size={isMobile ? 'lg' : 'xl'} order={2} c={theme.colors.celesteGold[5]}>
             {product.title}
           </Title>
-          <Title order={2} c="gray.7">
+          <Title size={isMobile ? 'lg' : 'xl'} order={2} c="gray.7">
             |
           </Title>
-          <Title order={2} c={theme.colors.celesteGold[5]}>
+          <Title size={isMobile ? 'lg' : 'xl'} order={2} c={theme.colors.celesteGold[5]}>
             {product.sku}
           </Title>
         </Flex>
-        <Text mt="md" c="gray.7">
+        <Text size={isMobile ? 'sm' : 'md'} mt="md" c="gray.7">
           {product.description}
         </Text>
       </Container>
